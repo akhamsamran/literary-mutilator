e1c82c4b1debdb69363698581b2f8a4197fc4932
Anna Khamsamran <akhamsamran1@cnm.edu>
2018-02-05 18:37:04 -0700

    change text color script

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+function changeTextColor() {
+	document.getElementById("paragraph1").style.color = "#6A266F";
+	document.getElementById("paragraph2").style.color = "#FF7400";
+	document.getElementById("paragraph3").style.color = "#27A200";
+	document.getElementById("named").style.color = "#074AA7";
+	document.getElementById("author").style.color = "#2911B1";
+};
